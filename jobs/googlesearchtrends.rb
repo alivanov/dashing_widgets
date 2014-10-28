910ed2e64b34fef19038a4b0d5eedae2d06c73f8
Alexey Ivanov <ivanov@intspirit.com>
2014-10-28 15:53:50 +0300

    set 1m interval, add commented 5m interval

--- jobs/googlesearchtrends.rb
+++ jobs/googlesearchtrends.rb
@@ -4,7 +4,8 @@ require 'json'
 uri = URI('https://www.kimonolabs.com/api/5pelyzn8?apikey=VGJ0nMhw7BsmCGs7UqxW8CuUW2B5Xoac')
 google_search_hash = Hash.new()
 
-SCHEDULER.every '15m' do
+#SCHEDULER.every '5m' do
+SCHEDULER.every '1m' do
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') { |http|
         request = Net::HTTP::Get.new uri
         response = http.request request
--- jobs/youtubetrends.rb
+++ jobs/youtubetrends.rb
@@ -4,6 +4,7 @@ require 'json'
 uri = URI('https://www.kimonolabs.com/api/dmzo8lei?apikey=6caa957c418cd29064574897317dc6d8')
 youtube_hash = Hash.new()
 
+#SCHEDULER.every '5m' do
 SCHEDULER.every '1m' do
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') { |http|
         request = Net::HTTP::Get.new uri
